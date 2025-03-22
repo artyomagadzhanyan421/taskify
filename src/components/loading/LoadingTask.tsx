80a96366bac2050dd9a7b33e05d506ab08ea41ea
artyomagadzhanyan421 <artyom.agadzhanyan421@gmail.com>
2025-03-22 17:50:00 +0500

    status commit

--- src/components/loading/LoadingTask.tsx
+++ src/components/loading/LoadingTask.tsx
@@ -13,6 +13,9 @@ function LoadingTask() {
                 <Skeleton height={30} width={234.33} borderRadius={6} baseColor={lightMode ? "#E4E4E7" : "#3F3F46"} highlightColor={lightMode ? "#D4D4D8" : "#52525B"} style={{ marginTop: 18, marginBottom: 20 }} />
                 <Skeleton height={20} count={3} borderRadius={3} baseColor={lightMode ? "#E4E4E7" : "#3F3F46"} highlightColor={lightMode ? "#D4D4D8" : "#52525B"} />
                 <Skeleton height={20} width={200} borderRadius={3} baseColor={lightMode ? "#E4E4E7" : "#3F3F46"} highlightColor={lightMode ? "#D4D4D8" : "#52525B"} />
+                <div className="functions" style={{ marginTop: 20 }}>
+                    <Skeleton height={55} width={216.7} borderRadius={8} baseColor={lightMode ? "#E4E4E7" : "#3F3F46"} highlightColor={lightMode ? "#D4D4D8" : "#52525B"} />
+                </div>
             </div>
         </div>
     )
